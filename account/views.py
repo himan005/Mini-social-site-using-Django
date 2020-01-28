791dadb7ae77b394467f49242ee20b3e9423d7ae
himan005 <himanshugoej@gmail.com>
2020-01-28 23:24:29 +0530

    Initial Commit

---
+++ account/views.py
@@ -0,0 +1,28 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+
+from .forms import LoginForm
+
+def user_login(request):
+    if request.method == "POST":
+        form =LoginForm(request.POST or None)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request, username = cd['username'], password = cd['password'])
+            if user is not None:
+                if user.is_active():
+                    login(request, user)
+                    return HttpResponse("Authentication Successfull")
+                else:
+                    return HttpResponse("Disabled Account")
+            else:
+                return HttpResponse('Invalid Login')
+    else:
+        form =LoginForm()
+        context = {
+            'form': form
+        }
+    return render(request, 'account/login.html', context)
+
+
